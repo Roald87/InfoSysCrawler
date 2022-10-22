b4d5a834ac632d36a34a226a598cdefe3e8f46a6
Roald Ruiter <roaldruiter@gmail.com>
2022-10-22 11:02:54 +0200

    capture version without major number

--- script/InfoSysCrawler.tests/Tests.fs
+++ script/InfoSysCrawler.tests/Tests.fs
@@ -181,6 +181,7 @@ let ``With TwinCAT version on page`` (fname) =
 [<InlineData("TwinCAT v3.1 Build 4022.30", "3.1.4022.30")>]
 [<InlineData("TwinCAT v3.1. >= 4022.31", "3.1.4022.31")>]
 [<InlineData("TwinCAT v3.1. > 4024.35", "3.1.4024.35")>]
+[<InlineData("TwinCAT v3.1", "3.1")>] // for example https://infosys.beckhoff.com/content/1033/tcplclib_tc2_system/3622991755.html?id=6591934602709167291
 let ``TwinCAT version with additional characters/text``
     (
         versionString,
